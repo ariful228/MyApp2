125a0cb096809de4f479f00a94f3c455e4434efd
Ariful Islam <ldapuser1@bjit.com>
2019-08-26 12:49:37 +0600

    Add name

--- app/src/main/java/com/example/myapp2/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/myapp2/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment @Hello");
+        mText.setValue("This is notifications fragment @Hello Ariful");
     }
 
     public LiveData<String> getText() {
